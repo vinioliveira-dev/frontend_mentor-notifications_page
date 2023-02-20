7d04cee878592deb1e84f3c279004939bb2ef780
Vinícius Queiróz de Oliveira <vqovinicius@gmail.com>
2023-02-20 16:28:04 +0100

    little update on page components

---
+++ notif-page-on-react/src/notif-page/NotifPage.js
@@ -0,0 +1,11 @@
+function NotifPage() {
+    return (
+        <div className="notif-page">
+            <div className="page-header">
+                <h1>Notifications</h1>
+                <p>3</p>
+                <button>Mark all as read</button>
+            </div>
+        </div>
+    )
+}
